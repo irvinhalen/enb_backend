40d4b5bbca8e4bf91184fdd0bfff87fa63c0a7cf
koyaIrvin <irvinhalen.larawan@gmail.com>
2024-03-27 15:34:48 +0800

    add transaction

--- app/Http/Controllers/TruckTransactionController.php
+++ app/Http/Controllers/TruckTransactionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TruckTransaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -23,7 +24,7 @@ class TruckTransactionController extends Controller
     public function select_data(Request $request){
         $user_id = $request->id;
         $options = DB::table('site_assignments')
-                    ->select('project_name', 'license_plate', 'sites.site_id')
+                    ->select('project_name', 'license_plate', 'sites.site_id', 'truck_id', 'weight_capacity')
                     ->join('sites', 'sites.site_id', '=', 'site_assignments.site_id')
                     ->join('trucks', 'trucks.site_id', '=', 'sites.site_id')
                     ->whereRaw('user_id = ?', [$user_id])
@@ -31,6 +32,25 @@ class TruckTransactionController extends Controller
                     ->get();
         return $options;
     }
-    
-    public function create(Request $request){}
+
+    public function create(Request $request){
+        $incomingFields = $request->validate([
+            'truck_id' => 'required',
+            'site_id' => 'required',
+            'soil_amount' => 'required',
+            'in' => 'required',
+            'out' => 'required',
+            'in_time' => 'required',
+            'out_time' => 'required'
+        ]);
+
+        DB::table('truck_transactions')->insert([
+            ['truck_id' => $incomingFields['truck_id'], 'site_id' => $incomingFields['site_id'], 'soil_amount' => $incomingFields['soil_amount'], 'in' => $incomingFields['in'], 'out' => $incomingFields['out'], 'in_time' => $incomingFields['in_time'], 'out_time' => $incomingFields['out_time']]
+        ]);
+
+        return [
+            'status' => 'success',
+            $incomingFields
+        ];
+    }
 }
