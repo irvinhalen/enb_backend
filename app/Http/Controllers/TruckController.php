330afc56ecb730590629490980940c97e4a0d861
koyaIrvin <irvinhalen.larawan@gmail.com>
2024-04-19 09:05:01 +0800

    truck index get by user id

--- app/Http/Controllers/TruckController.php
+++ app/Http/Controllers/TruckController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class TruckController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+        $user_id = $request->id;
         $trucks = DB::table('trucks')
                     ->select('license_plate', 'project_name', 'beacon_name', 'weight_capacity')
-                    ->join('sites', 'sites.site_id', '=', 'trucks.site_id')
-                    ->join('beacons', 'beacons.beacon_id', '=', 'trucks.beacon_id')
+                    ->leftJoin('sites', 'sites.site_id', '=', 'trucks.site_id')
+                    ->join('site_assignments', 'site_assignments.site_id', '=', 'sites.site_id')
+                    ->leftJoin('beacons', 'beacons.beacon_id', '=', 'trucks.beacon_id')
+                    ->whereRaw('user_id = ?', [$user_id])
                     ->get();
         return $trucks;
     }
